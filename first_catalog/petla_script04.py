6afe912857d43de386d703b7c462ff7bcb92eb8d
CSComarch <alicja.kozlowska87@gmail.com>
2023-02-21 12:29:50 +0100

    petla03_alfabet2_change

---
+++ first_catalog/petla_script04.py
@@ -0,0 +1,33 @@
+#petla iteracyjna + literki
+#od a do z
+#wersja pierwsza
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+# for letter in alphabet:
+#     print(f"Letter is {letter}")
+#
+# for letter in alphabet:
+#     print(f"Letter is {letter}", sep=" ")
+#     print(letter, end=" / ")
+
+# zbior = (1, 3, 5, 7, 9)
+# a, b, c, e, d = zbior
+# print(c)
+
+# colors = ("red", "blue", "green")
+# for color in colors:
+#     print(color)
+
+# for letter in enumerate(alphabet):
+#     print(f"Letter is {letter}")
+#
+# for position, letter in enumerate(alphabet):
+#     print(f"Letter is {letter}, {position=}")
+#     if position == 7:
+#         print("Siódemka!!!!")
+
+#wersja druga
+
+for number in range(97, 123):
+    print(f"Letter is {chr(number)}")

298e659c89c32c7162b8fbf9bbc3033501d8a8ad
CSComarch <alicja.kozlowska87@gmail.com>
2023-02-21 13:11:59 +0100

    petla04_while_change

--- first_catalog/petla_script04.py
+++ first_catalog/petla_script04.py
@@ -1,33 +1,6 @@
-#petla iteracyjna + literki
-#od a do z
-#wersja pierwsza
+#petla iteracyjna + while
+inc = 0
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-# for letter in alphabet:
-#     print(f"Letter is {letter}")
-#
-# for letter in alphabet:
-#     print(f"Letter is {letter}", sep=" ")
-#     print(letter, end=" / ")
-
-# zbior = (1, 3, 5, 7, 9)
-# a, b, c, e, d = zbior
-# print(c)
-
-# colors = ("red", "blue", "green")
-# for color in colors:
-#     print(color)
-
-# for letter in enumerate(alphabet):
-#     print(f"Letter is {letter}")
-#
-# for position, letter in enumerate(alphabet):
-#     print(f"Letter is {letter}, {position=}")
-#     if position == 7:
-#         print("Siódemka!!!!")
-
-#wersja druga
-
-for number in range(97, 123):
-    print(f"Letter is {chr(number)}")
+while inc < 6:
+    print(f"{inc=}")
+    inc += 1

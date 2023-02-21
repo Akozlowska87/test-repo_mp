55a1fe17086f8ec02c6710e30ddf69cd6a284bc6
CSComarch <alicja.kozlowska87@gmail.com>
2023-02-21 09:48:34 +0100

    first_change

---
+++ first_catalog/first_script.py
@@ -0,0 +1,12 @@
+current_year = 2023
+my_birthday = 1987
+my_age = current_year - my_birthday
+print(f"My age is {my_age}")
+
+if my_age >= 18:
+    print("You are adult")
+    print("You are inteligent")
+elif my_age < 0:
+    print("ERROR")
+else:
+    print("You aren't young")

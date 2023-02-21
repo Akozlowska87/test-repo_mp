cdae31b30e024dfa8f0c4e89c5894e50b6cb7075
CSComarch <alicja.kozlowska87@gmail.com>
2023-02-21 12:37:10 +0100

    petla03_alfabet3_change

--- first_catalog/petla_script03.py
+++ first_catalog/petla_script03.py
@@ -3,6 +3,18 @@
 #wersja pierwsza
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet[4]
+alphabet[3:10]
+print(alphabet[3:10:2])
+print(alphabet[::-1])
+print(alphabet[::-2])
+print(alphabet[::-3])
+print(alphabet[:-1])
+print(alphabet[:-2])
+print(alphabet[:-3])
+print(alphabet[-1:])
+print(alphabet[-2:])
+print(alphabet[-3:])
 
 # for letter in alphabet:
 #     print(f"Letter is {letter}")
@@ -19,10 +31,15 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 # for color in colors:
 #     print(color)
 
-for letter in enumerate(alphabet):
-    print(f"Letter is {letter}")
+# for letter in enumerate(alphabet):
+#     print(f"Letter is {letter}")
+#
+# for position, letter in enumerate(alphabet):
+#     print(f"Letter is {letter}, {position=}")
+#     if position == 7:
+#         print("Siódemka!!!!")
 
-for position, letter in enumerate(alphabet):
-    print(f"Letter is {letter}, {position=}")
-    if position == 7:
-        print("Siódemka!!!!")
+# wersja druga
+#
+# for number in range(97, 123):
+#     print(f"Letter is {chr(number)}")

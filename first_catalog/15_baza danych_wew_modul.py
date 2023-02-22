181e43e7c939df126538484b940f2af7f30937f2
CSComarch <alicja.kozlowska87@gmail.com>
2023-02-22 15:17:02 +0100

    nowy_plik_md

--- first_catalog/15_baza danych_wew_modul.py
+++ first_catalog/15_baza danych_wew_modul.py
@@ -2,7 +2,7 @@ import csv
 from faker import Faker
 ludzik = Faker("pl-PL")
 
-with open("ludzie.csv", "w" , newline="") as plik_csv:
+with open("pliki/ludzie.csv", "w" , newline="") as plik_csv:
     csv_writer = csv.writer(plik_csv, quoting=csv.QUOTE_NONNUMERIC) #delimiter=" ", quotechar="|",
     for _ in range(101):
         csv_writer.writerow([ludzik.name(), ludzik.company(), ludzik.company_vat(), ludzik.local_regon()])
---
+++ first_catalog/17_pandas.py
@@ -0,0 +1,4 @@
+import snakemd
+
+nowy_dokument = snakemd.new_doc("pliki/nowy_plik")
+nowy_dokument.output_page()

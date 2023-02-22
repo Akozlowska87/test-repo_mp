323443850a6c251f19825a5a3104f8595666f0d0
CSComarch <alicja.kozlowska87@gmail.com>
2023-02-22 13:34:16 +0100

    baza_csv

--- first_catalog/15_baza danych_wew_modul.py
+++ first_catalog/15_baza danych_wew_modul.py
@@ -3,6 +3,6 @@ from faker import Faker
 ludzik = Faker("pl-PL")
 
 with open("ludzie.csv", "w" , newline="") as plik_csv:
-    csv_writer = csv.writer(plik_csv, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL)
-    for _ in range(10):
+    csv_writer = csv.writer(plik_csv, quoting=csv.QUOTE_NONNUMERIC) #delimiter=" ", quotechar="|",
+    for _ in range(101):
         csv_writer.writerow([ludzik.name(), ludzik.company(), ludzik.company_vat(), ludzik.local_regon()])

6604b37fe0db60cfbc5d02dbb70b3af0f5832461
CSComarch <alicja.kozlowska87@gmail.com>
2023-02-22 15:25:58 +0100

    nowy_plik_generyczna_nawza_md

--- first_catalog/17_pandas.py
+++ first_catalog/17_pandas.py
@@ -1,4 +1,9 @@
 import snakemd
+#dodanie pustego pliku
+# nowy_dokument = snakemd.new_doc("pliki/nowy_plik")
+# nowy_dokument.output_page()
 
-nowy_dokument = snakemd.new_doc("pliki/nowy_plik")
+from secrets import token_urlsafe
+plik = f"pliki/{token_urlsafe(8)}"
+plik_dokument = snakemd.new_doc(pliki)
 nowy_dokument.output_page()

9500343b6595a5e7da54b218df5e51f81de661b2
CSComarch <alicja.kozlowska87@gmail.com>
2023-02-22 11:05:03 +0100

    wielolinijkowy kom i zmienna

---
+++ first_catalog/0000_obiekt tekstowy.py
@@ -0,0 +1,21 @@
+zmienna1 = "Ala ma kota"
+zmienna2 = "a kot ma Alę"
+
+#z przypisaniem do zmiennej
+zmienna3 = """
+Ala ma kota
+a kot ma Alę
+a Ala ma 
+   w głowie Kota
+UWAGA na Alę!
+"""
+print(zmienna3)
+
+# wielolinijkowy komentarz
+"""
+Ala ma kota
+a kot ma Alę
+a Ala ma 
+   w głowie Kota
+UWAGA na Alę!
+"""

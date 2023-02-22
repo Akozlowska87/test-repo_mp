c72b32bc1aaa5f201d69a02913cd000f7fbc4ed1
CSComarch <alicja.kozlowska87@gmail.com>
2023-02-22 15:57:24 +0100

    nowy_plik_generyczna_nawza2idoc_md

--- first_catalog/17_pandas_generowaniepliku.py
+++ first_catalog/17_pandas_generowaniepliku.py
@@ -1,10 +1,14 @@
 import snakemd
+from secrets import token_urlsafe
+import subprocess
+
 #dodanie pustego pliku
 # nowy_dokument = snakemd.new_doc("pliki/nowy_plik")
 # nowy_dokument.output_page()
 
-from secrets import token_urlsafe
+
 plik = f"pliki/pliki_generowne/{token_urlsafe(8)}"
+
 plik_dokument = snakemd.new_doc(plik)
 
 tekst = """
@@ -18,3 +22,7 @@ plik_dokument.add_horizontal_rule()
 plik_dokument.add_paragraph(tekst)
 plik_dokument.add_code("from snakemd import *", lang="python")
 plik_dokument.output_page()
+
+command = ["pandoc", "-o", f"{plik}.docx", f"{plik}.md"]
+ret_code = subprocess.run(command, capture_output=True)
+print(ret_code)

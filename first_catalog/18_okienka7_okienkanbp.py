ca98ef7365e3977944ce7b9295587b7e4bb52921
CSComarch <alicja.kozlowska87@gmail.com>
2023-02-23 12:04:13 +0100

    rysowanie_wykres_doc

---
+++ first_catalog/18_okienka7_okienkanbp.py
@@ -0,0 +1,82 @@
+# https://www.pysimplegui.org/en/latest/
+import PySimpleGUI as sg
+import requests
+import snakemd
+import subprocess
+import os
+from secrets import token_urlsafe
+from pathlib import Path
+from datetime import date, timedelta
+
+TEMP_DIR = "pliki_tymczasowe"
+NBP_URL= "https://api.nbp.pl/"
+TODAY = date.today()
+TODAY_ISO = TODAY.isoformat()
+
+def init_dir():
+    if not Path(TEMP_DIR).exists():
+        os.mkdir(TEMP_DIR)
+
+def generuj_dokument(code, start_date, end_date=TODAY_ISO):
+    plik = f"{TEMP_DIR}/{token_urlsafe(8)}"
+    url_address = f"http://api.nbp.pl/api/exchangerates/rates/A/{code}/{start_date}/{end_date}/"
+    plik_dokument = snakemd.new_doc(plik)
+    try:
+        plik_dokument.output_page()
+    except Exception as e:
+        sg.Popup(e, title="ERROR")
+
+def test_inna():
+    try:
+        nbp_test = requests.get(NBP_URL)
+        status_nbp = nbp_test.status_code
+    except Exception as e:
+        sg.Popup(f"Error-> {e} | status_code -> -999", title="ERROR")
+        status_nbp = -999
+
+    if status_nbp == 200:
+        sg.Popup(f"Połączono z {NBP_URL} - status OK")
+    elif status_nbp == -999:
+        sg.Popup(f"Brak interentu - zweryfikuja połączenie {NBP_URL} - sprawdź kabelek")
+
+    else:
+        sg.Popup(f"Brak internetu - zweryfikuja połączenie {NBP_URL}- code: {status_nbp}")
+
+#start aplikacji
+init_dir()
+
+# definiujemy wygląd aplikacji
+app_layout = [
+    [sg.Text("Jak wygląda wykres wartości waluty w czasie?")],
+    [sg.Text("Jeżeli chcesz zobaczyc wykres wpisz poniższe dane i kliknij OK, jeżeli chcesz zamknąć okno wybierz EXIT")],
+    [sg.Text("UWAGA! - wykres może dotyczyć 30 dni wstecz.")],
+    [sg.Text("Podaj kod waluty (np.: 'CHF'"), sg.Input("Wpisz wartość")],
+    [sg.Text("Ile dni wstecz wykres, max 90?"), sg.Input("")],
+    [sg.OK(), sg.Exit(), sg.Button("Inna"), sg.Button("Get waluta")]
+]
+window = sg.Window("Twój poranek", app_layout)
+
+# używamy pętli nieskończonej, która działa aż do słowa kluczowego `break`
+# pamiętajmy o PEP-8, wcięciach i bloku kodu - https://www.python.org/dev/peps/pep-0008/#indentation
+while True:
+    # poniższe wywołanie otwiera okno i wczytuje dane
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == "Exit":
+        print("Użytkownik zamknął okno przeglądarki lub kliknął 'Exit'")
+        break
+
+    if event == "OK":
+        waluta = values[0].upper()
+        # https://docs.python.org/3/library/datetime.html#examples-of-usage-timedelta
+        data_poczatkowa = TODAY - timedelta(int(values[1]))
+        generuj_dokument()
+
+    if event == "Inna":
+        test_inna()
+
+    # sprawdzamy wartości zwracane przez okno
+    sg.popup(f"Zdarzenie to:{event}, zwracany słownik to: {values}")
+
+# koniec programu
+window.close()
+print("End of application")

cc9c4ca574f3a402b8880aac854528018d28fa2a
CSComarch <alicja.kozlowska87@gmail.com>
2023-02-23 09:17:09 +0100

    jedno_okno

---
+++ first_catalog/18_okienka.py
@@ -0,0 +1,5 @@
+# https://www.pysimplegui.org/en/latest/
+import PySimpleGUI as sg
+
+# proste okno z informacją i zdefiniowanym tytułem
+sg.popup("Hej, ale dzisiaj ładnie wyglądasz.", title="Piekna i bestia")

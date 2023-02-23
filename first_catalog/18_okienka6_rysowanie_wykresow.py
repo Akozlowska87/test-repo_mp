9eeb826d57186683d65e287bfe513744332f13a9
CSComarch <alicja.kozlowska87@gmail.com>
2023-02-23 11:39:24 +0100

    rysowanie_wykresu

---
+++ first_catalog/18_okienka6_rysowanie_wykresow.py
@@ -0,0 +1,25 @@
+import requests
+import matplotlib.pyplot as plt
+
+table = "A"
+code = "EUR"
+startDate = "2022-11-01"
+endDate = "2023-02-21"
+url_address = f"http://api.nbp.pl/api/exchangerates/rates/{table}/{code}/{startDate}/{endDate}/"
+
+kursy = []
+req_get = requests.get(url_address)
+dane = req_get.json()
+# print(dane['rates'])
+for dane_z_dnia in dane['rates']:
+    kurs_z_dnia = dane_z_dnia['mid']
+    kursy.append(kurs_z_dnia)
+
+print(kursy)
+
+X = [ x for x in range(len(kursy))]
+plt.plot(X,kursy)
+# plt.bar(X,kursy)
+# plt.barh(kursy, X)
+plt.grid()
+plt.show()

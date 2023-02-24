5902b834bd77df58728894b092fa78d67d4512c6
CSComarch <alicja.kozlowska87@gmail.com>
2023-02-24 10:28:28 +0100

    wysylka_maila_na_mojego

--- first_catalog/stronka/03_route_parametry.py
+++ first_catalog/stronka/03_route_parametry.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from funkcje_dodatkowe.baza_danych import generate_token
 from funkcje_dodatkowe.send_email_smarthost import mail_report
+from datetime import datetime
 
 app = Flask("moja_apka)")
 zmienna_m = 4
@@ -16,12 +17,16 @@ def get_data():
     """ + str(generate_token())
     return html
 
-@app.route("/user")
-def username():
-    html = """
+@app.route("/user/<value>")
+def username(value):
+    html = f"""
     <h1>Welcome new user</h1>
-    Wyślę Ci maila na adres:
+    Wysyłam Ci maila na adres: {value}
     """
+    if value.count("@") == 1:
+        ret, ret_value = mail_report(value, "python-course@jurkiewicz.tech", f"treść naszego maila - {datetime.today()}")
+        if ret == False:
+            print(ret_value)
     return html
 
 app.run()
